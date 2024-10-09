c581462c9c655ac9a00fb9f6aebba08ab993791b
ryanmaguire <rmaguire@protonmail.com>
2024-10-09 17:09:20 -0400

    More cleaning up.

--- functions/fresnel/fresnel_auxiliary.py
+++ functions/fresnel/fresnel_auxiliary.py
@@ -1,49 +1,66 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of libtmpl_data.                                         #
+#                                                                              #
+#   libtmpl_data is free software: you can redistribute it and/or modify it    #
+#   under the terms of the GNU General Public License as published by          #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl_data is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with libtmpl_data.  If not, see <https://www.gnu.org/licenses/>.     #
+################################################################################
+#   Purpose:                                                                   #
+#       Remez coefficients for the auxiliary Fresnel functions f and g.        #
+################################################################################
+#   Author: Ryan Maguire                                                       #
+#   Date:   May 23, 2024.                                                      #
+################################################################################
+"""
 import tmpld
 import tmpld.remez
-tmpld.mpmath.mp.prec = 300
-PI_FACTOR = tmpld.mpmath.sqrt(tmpld.mpmath.pi()) / 2
-PLUS_SCALE = tmpld.mpmath.mpc(1 + 1j)
-MINUS_SCALE = tmpld.mpmath.mpc(1 - 1j)
-HALF = tmpld.mpmath.mpf(0.5)
+from fresnel_auxiliary_f import auxiliary_f
+from fresnel_auxiliary_g import auxiliary_g
 
-def fresnel_cos(x):
-    factor = tmpld.mpmath.mpf(x) * PI_FACTOR
-    left = MINUS_SCALE * tmpld.mpmath.erf(PLUS_SCALE * factor)
-    right = PLUS_SCALE * tmpld.mpmath.erf(MINUS_SCALE * factor)
-    return (left + right).real / 4
+def transform(x_val):
+    """
+        Function:
+            transform
+        Purpose:
+            Transforms the interval [1, infinity) to (0, 1] using y = 1 / x.
+    """
+    x_mpf = tmpld.mpmath.mpf(x_val)
+    return tmpld.mpmath.mpf(8) / (x_mpf + 1)
 
-def fresnel_sin(x):
-    factor = tmpld.mpmath.mpf(x) * PI_FACTOR
-    left = PLUS_SCALE * tmpld.mpmath.erf(PLUS_SCALE * factor)
-    right = MINUS_SCALE * tmpld.mpmath.erf(MINUS_SCALE * factor)
-    return (left + right).real / 4
+def tranformed_f(x_val):
+    """
+        Function:
+            transformed_f
+        Purpose:
+            Computes the auxiliary "f" functions for
+            the transformed variable "x".
+    """
+    return auxiliary_f(transform(x_val))
 
-def ff(x):
-    factor = tmpld.mpmath.mpf(x)
-    A = fresnel_cos(factor) - HALF
-    B = fresnel_sin(factor) - HALF
-    a = tmpld.mpmath.sin(tmpld.mpmath.pi() * factor * factor * HALF)
-    b = tmpld.mpmath.cos(tmpld.mpmath.pi() * factor * factor * HALF)
-    return a*A - b*B
-
-def fg(x):
-    factor = tmpld.mpmath.mpf(x)
-    A = fresnel_cos(factor) - HALF
-    B = fresnel_sin(factor) - HALF
-    a = tmpld.mpmath.sin(tmpld.mpmath.pi() * factor * factor * HALF)
-    b = tmpld.mpmath.cos(tmpld.mpmath.pi() * factor * factor * HALF)
-    return -a*B - A*b
-
-def t(x):
-    y = tmpld.mpmath.mpf(x)
-    return tmpld.mpmath.mpf(8) / (y + 1)
-
-
-def f(x):
-    return fg(t(x))
+def tranformed_g(x_val):
+    """
+        Function:
+            transformed_g
+        Purpose:
+            Computes the auxiliary "g" functions for
+            the transformed variable "x".
+    """
+    return auxiliary_g(transform(x_val))
 
 START = 2**-15 - 1
 END = 1.0
 
-(P, Q, e) = tmpld.remez.rat_remez(f, 8, 8, START, END)
+(P, Q, e) = tmpld.remez.rat_remez(tranformed_f, 8, 8, START, END)
 tmpld.remez.print_rat_coeffs(P, Q)
--- functions/fresnel/fresnel_auxiliary_small.py
+++ functions/fresnel/fresnel_auxiliary_small.py
@@ -1,45 +1,66 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of libtmpl_data.                                         #
+#                                                                              #
+#   libtmpl_data is free software: you can redistribute it and/or modify it    #
+#   under the terms of the GNU General Public License as published by          #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   libtmpl_data is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with libtmpl_data.  If not, see <https://www.gnu.org/licenses/>.     #
+################################################################################
+#   Purpose:                                                                   #
+#       Remez coefficients for the auxiliary Fresnel functions f and g.        #
+################################################################################
+#   Author: Ryan Maguire                                                       #
+#   Date:   May 23, 2024.                                                      #
+################################################################################
+"""
 import tmpld
 import tmpld.remez
-tmpld.mpmath.mp.prec = 300
-PI_FACTOR = tmpld.mpmath.sqrt(tmpld.mpmath.pi()) / 2
-PLUS_SCALE = tmpld.mpmath.mpc(1 + 1j)
-MINUS_SCALE = tmpld.mpmath.mpc(1 - 1j)
-HALF = tmpld.mpmath.mpf(0.5)
+from fresnel_auxiliary_f import auxiliary_f
+from fresnel_auxiliary_g import auxiliary_g
 
-def fresnel_cos(x):
-    factor = tmpld.mpmath.mpf(x) * PI_FACTOR
-    left = MINUS_SCALE * tmpld.mpmath.erf(PLUS_SCALE * factor)
-    right = PLUS_SCALE * tmpld.mpmath.erf(MINUS_SCALE * factor)
-    return (left + right).real / 4
+def transform(x_val):
+    """
+        Function:
+            transform
+        Purpose:
+            Transforms the interval [1, infinity) to (0, 1] using y = 1 / x.
+    """
+    x_mpf = tmpld.mpmath.mpf(x_val)
+    return tmpld.mpmath.mpf(1) / x_mpf
 
-def fresnel_sin(x):
-    factor = tmpld.mpmath.mpf(x) * PI_FACTOR
-    left = PLUS_SCALE * tmpld.mpmath.erf(PLUS_SCALE * factor)
-    right = MINUS_SCALE * tmpld.mpmath.erf(MINUS_SCALE * factor)
-    return (left + right).real / 4
+def tranformed_f(x_val):
+    """
+        Function:
+            transformed_f
+        Purpose:
+            Computes the auxiliary "f" functions for
+            the transformed variable "x".
+    """
+    return auxiliary_f(transform(x_val))
 
-def ff(x):
-    factor = tmpld.mpmath.mpf(x)
-    A = fresnel_cos(factor) - HALF
-    B = fresnel_sin(factor) - HALF
-    a = tmpld.mpmath.sin(tmpld.mpmath.pi() * factor * factor * HALF)
-    b = tmpld.mpmath.cos(tmpld.mpmath.pi() * factor * factor * HALF)
-    return a*A - b*B
-
-def fg(x):
-    factor = tmpld.mpmath.mpf(x)
-    A = fresnel_cos(factor) - HALF
-    B = fresnel_sin(factor) - HALF
-    a = tmpld.mpmath.sin(tmpld.mpmath.pi() * factor * factor * HALF)
-    b = tmpld.mpmath.cos(tmpld.mpmath.pi() * factor * factor * HALF)
-    return -a*B - A*b
-
-def f(x):
-    xval = 1 / tmpld.mpmath.mpf(x)
-    return fg(xval)
+def tranformed_g(x_val):
+    """
+        Function:
+            transformed_g
+        Purpose:
+            Computes the auxiliary "g" functions for
+            the transformed variable "x".
+    """
+    return auxiliary_g(transform(x_val))
 
 START = 0.25
 END = 0.5
 
-(P, Q, e) = tmpld.remez.rat_remez(f, 28, 3, START, END)
+(P, Q, e) = tmpld.remez.rat_remez(tranformed_f, 28, 3, START, END)
 tmpld.remez.print_rat_coeffs(P, Q)
